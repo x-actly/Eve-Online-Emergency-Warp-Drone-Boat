f9e552de66de76dbdf7750eeb9768fe22cfccd8f
x-actly <80623408+x-actly@users.noreply.github.com>
2023-10-16 14:30:22 +0200

    +

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pyautogui
-import sys
+import pyautogui, sys, random, time
+
+
 
 def find_and_execute(image_paths, action_function, start_x, start_y, end_x, end_y, confidence):
     search_region = (start_x, start_y, end_x - start_x, end_y - start_y)
@@ -17,18 +18,23 @@ def find_and_execute(image_paths, action_function, start_x, start_y, end_x, end_
 
 def perform_action():
     # Hier können Sie Ihre eigene Funktionslogik einfügen, die ausgeführt wird, wenn eines der Bilder gefunden wird.
-    print("Bild gefunden, Aktion wird ausgeführt.")
+    print("warping to safespot")
     sys.exit()
 
-start_x, start_y = 374, 605  # Beispiel Startpunkt der Suchregion
-end_x, end_y = 497, 1323  # Beispiel Endpunkt der Suchregion
+start_x, start_y = 387, 464  # Beispiel Startpunkt der Suchregion
+end_x, end_y = 562, 907  # Beispiel Endpunkt der Suchregion
 #image_paths = ['img/neut_4k.png', 'img/red_4k.png']  # Beispiel Pfade zu den Bildern
 image_paths = ['img/neut_1080p.png', 'img/red_1080p.png']
-confidence_level = 0.88  # Beispielwert für die Intensität der Bilderkennung
+confidence_level = 0.91  # Beispielwert für die Intensität der Bilderkennung
+
 
 while True:
+    
+    random_intervall = random.uniform(1, 2)
+
     found = find_and_execute(image_paths, perform_action, start_x, start_y, end_x, end_y, confidence_level)
-    print("Bilder werden gesucht!")
+    time.sleep(random_intervall)
+    print("local clear!", random_intervall)
     if found:
         perform_action()
         break
