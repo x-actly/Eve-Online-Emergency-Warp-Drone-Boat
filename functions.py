e7b64cab742d83bd252a11cc47a14d8b9b0d0d0f
x-actly <80623408+x-actly@users.noreply.github.com>
2023-10-17 03:57:29 +0200

    +

--- functions.py
+++ functions.py
@@ -3,6 +3,16 @@ import pyautogui
 
 
 
+def targeting(x, y):
+
+    pyautogui.moveTo(x, y)
+    pyautogui.keyDown('ctrl')
+    pyautogui.click(button='left')
+    time.sleep(1)
+    pyautogui.click(button='left')
+    pyautogui.keyUp('ctrl')
+    time.sleep(3)
+    pyautogui.press('f')
 
 
 def emergency_warp(x, y):
@@ -12,8 +22,6 @@ def emergency_warp(x, y):
     y_offset_warp = random.randint(-51,-49)
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     
-    # console
-    print(f"[{timestamp}] - emergency warp...")
     # warp to belt
     for i in range(1):
 
@@ -26,16 +34,16 @@ def emergency_warp(x, y):
         # pyautogui.keyUp('shift')
         # time.sleep(5)
         ######### align #############
-        time.sleep(random_time)
-        pyautogui.press('f3')
-        pyautogui.moveTo(x, y)
-        pyautogui.mouseDown()
-        time.sleep(random_time)
-        pyautogui.moveRel(-45, y_offset_align, 1)
-        pyautogui.mouseUp()
-        time.sleep(5)
+        # time.sleep(random_time)
+        # pyautogui.press('f3')
+        # pyautogui.moveTo(x, y)
+        # pyautogui.mouseDown()
+        # time.sleep(random_time)
+        # pyautogui.moveRel(-45, y_offset_align, 1)
+        # pyautogui.mouseUp()
+        # time.sleep(5)
         ######### warp ############## 
-        #time.sleep(random_time)
+        time.sleep(random_time)
         pyautogui.moveTo(x, y)
         pyautogui.mouseDown()
         time.sleep(random_time)
--- main.py
+++ main.py
@@ -1,8 +1,28 @@
-import pyautogui, sys, random, time, pygame, time
+import pyautogui, sys, random, time, pygame, time, threading
 from datetime import datetime
 from functions import emergency_warp
+from functions import targeting
 
 alarm_played = False
+
+additional_thread_running = True
+
+# Detection Area
+start_x, start_y = 357, 705  
+end_x, end_y = 487, 1309
+
+# safespot
+safe_x, safe_y = 1700, 1068
+
+# targeting
+targeting_x, targeting_y = 2048, 804
+
+# Image Path
+image_paths = ['img/neut_4k.png', 'img/red_4k.png', 'img/-5.png', 'img/-10.png']  
+#image_paths = ['img/neut_1080p.png', 'img/red_1080p.png', 'img/-5.png', 'img/-10.png']
+
+# Confidence Lvl - Laptop = 0.84 at 1080p, PC = 0.85 at 1440p
+confidence_level = 0.85 
 
 def find_and_execute(image_paths, action_function, start_x, start_y, end_x, end_y, confidence):
     search_region = (start_x, start_y, end_x - start_x, end_y - start_y)
@@ -14,7 +34,21 @@ def find_and_execute(image_paths, action_function, start_x, start_y, end_x, end_
             return True
     return False
 
+def additional_thread_function():
+    random_interval = random.uniform(60, 70)
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    global additional_thread_running
+    while additional_thread_running:
+        
+        time.sleep(random_interval)
+        print(f"{current_time} - targeting")
+        targeting(targeting_x, targeting_y)
 
+
+
+additional_thread = threading.Thread(target=additional_thread_function)
+additional_thread.daemon = True 
+additional_thread.start()
 
 def perform_action():
     global alarm_played
@@ -23,27 +57,20 @@ def perform_action():
         pygame.mixer.music.load('sound/alarm.mp3')
         pygame.mixer.music.play()
         current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        print(f"warping to safespot at {current_time}")
+        print(f"{current_time} - emergency warp")
         while pygame.mixer.music.get_busy():
             continue
         alarm_played = True
-        emergency_warp(659, 340)
+        emergency_warp(safe_x, safe_y)
 
     sys.exit()
-
-
-start_x, start_y = 232, 430  
-end_x, end_y = 348, 902  
-#image_paths = ['img/neut_4k.png', 'img/red_4k.png']  
-image_paths = ['img/neut_1080p.png', 'img/red_1080p.png']
-confidence_level = 0.84 
-
 
 while True:
     random_interval = random.uniform(1, 2)
     found = find_and_execute(image_paths, perform_action, start_x, start_y, end_x, end_y, confidence_level)
     current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')  
-    print(f"local clear at {current_time}")
+    print(f"{current_time} - local clear")
     time.sleep(random_interval)
     if found:
+        additional_thread_running = False
         break
